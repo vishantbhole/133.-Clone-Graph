9de3178853da65ca72ff51d13c3cd26eba32f7c0
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-15 00:03:39 -0600

    Update cloneGraph.py

--- cloneGraph.py
+++ cloneGraph.py
@@ -8,3 +8,7 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
     def __repr__(self):
         return f"Node({self.val})"
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        oldToNew: Dict[Node, Node] = {}
