d47b317c07ba82f3700cb90e8aa8335a47765024
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-15 00:03:13 -0600

    Update cloneGraph.py

--- cloneGraph.py
+++ cloneGraph.py
@@ -1,2 +1,10 @@
 
 #133. Clone Graph
+from typing import Optional, List, Dict
+
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+    def __repr__(self):
+        return f"Node({self.val})"
